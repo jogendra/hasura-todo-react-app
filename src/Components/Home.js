e3e11a703f47cc981cad3f29644a44d93d70dec8
Jogendra Singh <imjog24@gmail.com>
2018-12-10 00:03:19 +0530

    Initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import GetTodos from './GetTodos';
+
+export var client;
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    const ACCESS_TOKEN = localStorage.getItem('access_token');
+    client = new ApolloClient({
+      uri: "https://hasura-todo-jogendra.herokuapp.com/v1alpha1/graphql",
+      headers: {
+        'Authorization': `Bearer ${ACCESS_TOKEN}`,
+      }
+    });
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+  render() {
+    const { isAuthenticated } = this.props.auth;
+    return (
+      <div className="container">
+        {
+          isAuthenticated() && (
+            <ApolloProvider client={client}>
+              <GetTodos />
+            </ApolloProvider>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default Home;
